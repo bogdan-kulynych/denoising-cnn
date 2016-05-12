28aac15bfbfc39758abcb178032f33e40adafb04
Bogdan Kulynych <hello@bogdankulynych.me>
2016-05-13 01:41:32 +0300

    Add Gaussian and Uniform noise

--- utils/filters.py
+++ utils/filters.py
@@ -1,7 +1,10 @@
-from PIL import ImageFilter
+import numpy as np
+
+from scipy import stats
+from PIL import Image, ImageFilter
 
 
-class Noop:
+class Noop(object):
     def apply(self, image):
         return image.copy()
 
@@ -13,3 +16,39 @@ class GaussianBlur(object):
     def apply(self, image):
         return image.filter(ImageFilter.GaussianBlur(self.radius))
 
+    def __repr__(self):
+        return 'GaussianBlur({self.radius})'.format(self=self)
+
+
+class GaussianNoise(object):
+    def __init__(self, alpha=0.5, loc=0, scale=255):
+        self.alpha = alpha
+        self.loc = loc
+        self.scale = scale
+        self.dist = stats.truncnorm(0, 255, loc, scale)
+
+    def apply(self, image):
+        width, height = image.size
+        noise = np.uint8(self.dist.rvs(size=(width, height, 3)))
+        overlay = Image.fromarray(noise)
+        result = Image.blend(image, overlay, self.alpha)
+        return result
+
+    def __repr__(self):
+        return ('GaussianNoise('
+               '{self.alpha}, {self.loc}, {self.scale})').format(self=self)
+
+
+class UniformNoise(object):
+    def __init__(self, alpha=0.5):
+        self.alpha = alpha
+
+    def apply(self, image):
+        width, height = image.size
+        noise = np.uint8(np.random.random(size=(width, height, 3)) * 255)
+        overlay = Image.fromarray(noise)
+        result = Image.blend(image, overlay, self.alpha)
+        return result
+
+    def __repr__(self):
+        return 'UniformNoise({self.alpha})'.format(self=self)
--- utils/tests/test_filters.py
+++ utils/tests/test_filters.py
@@ -1,10 +1,12 @@
 import os
+import itertools
+
 import numpy as np
 import pytest
 
 from PIL import Image
 
-from utils.filters import Noop, GaussianBlur
+from utils.filters import Noop, GaussianBlur, GaussianNoise, UniformNoise
 
 
 @pytest.fixture()
@@ -13,11 +15,21 @@ def image(scope='module'):
     return Image.open(image_path)
 
 
+GAUSSIAN_BLUR_PARAMS = [1, 2, 5]
+GAUSSIAN_NOISE_PARAMS = list(itertools.product(
+    [0.1, 0.5, 0.9],
+    [0, 128, 255],
+    [10, 100, 200]))
+UNIFORM_NOISE_PARAMS = [0.1, 0.5, 0.9]
+
+
+# Very basic test to check if a filter changes anything in
+# an image
 @pytest.mark.parametrize("filter_instance",
-        # [Noop(), GaussianBlur(0)] +
-        [GaussianBlur(radius) for radius in [1, 2, 5, 10]]
-    )
+        [GaussianBlur(radius) for radius in GAUSSIAN_BLUR_PARAMS] +
+        [GaussianNoise(*args) for args in GAUSSIAN_NOISE_PARAMS] +
+        [UniformNoise(alpha) for alpha in UNIFORM_NOISE_PARAMS])
 def test_filter_changes_image(filter_instance, image):
-    altered_image = filter_instance.apply(image)
-    assert image.tobytes() != altered_image.tobytes()
+    image_after_filter = filter_instance.apply(image)
+    assert image.tobytes() != image_after_filter.tobytes()
 
